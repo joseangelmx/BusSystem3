9e2a6baa39a410737481693ba37e7d9f6e1fa1ca
Jose Angel Vazquez Torres <78801951+joseangelmx@users.noreply.github.com>
2026-05-07 23:58:29 -0600

    Add Swagger/OpenAPI services to the application

--- BusSystem/Program.cs
+++ BusSystem/Program.cs
@@ -35,6 +35,7 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
